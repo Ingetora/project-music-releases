40906b053de675238cce1800d78718e7b2c615bf
Ingetora <maria.bergman@outlook.com>
2021-03-13 16:36:50 +0100

    Gosh! Deleted repo and tried again!

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import data from './data.json'
+import { Album } from './components/Album'
+import { Artist } from './components/Artist'
 
-console.log(data)
+const albums = data.albums.items;
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
+  return ( 
+    <>
+    <header>
+      <h1>New albums &amp; singles</h1>
+    </header>
+    <div className = "main"> {
+      albums.map((album) => { 
+        return <Album
+        key = {album.name}
+        albumLink = {album.external_urls.spotify}
+        cover = {album.images[1].url}
+        title = {album.name}
+        artistName = {album.artists.map((artist) => ( 
+          <Artist
+          key = {artist.name}
+          artistName = {artist.name}
+          artistLink = {artist.external_urls.spotify}
+          />
+      ))}
+        />
+      })
+    } 
+    
     </div>
+    </>
   )
 }
